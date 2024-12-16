04d8a896d29062350c7f6949b60379878bf5889c
GameKnave <git@gameknave.com>
2024-12-16 05:40:34 -0800

    draft 12.1 chaosShared asset shuffle

--- script_archive/rulebook_scripts/examine_wh40k10e.js
+++ script_archive/rulebook_scripts/examine_wh40k10e.js
@@ -40,35 +40,104 @@ const fileList = [
   'World_Eaters.rulebook'
 ];
 async function processFiles() {
-  for (const file of fileList) {
+  // for (const file of fileList) {
     try {
-      const response = await fetch('../../' + file);
-      const data = await response.json();
-      data.revision = '10.12.0';
+      let response = await fetch('../../Space_Marines.rulebook');// + file);
+      let SM = await response.json();
+      response = await fetch('../../Chaos_Space_Marines.rulebook');// + file);
+      let CSM = await response.json();
+      response = await fetch('../../Chaos.rulebook');// + file);
+      let Chaos = await response.json();
+      Chaos.revision = '10.12.1';
 
-      console.log('~~~ ' + file + ' ~~~');
-      Object.entries(data.rulebook.assetCatalog).forEach(([itemKey, item]) => {
+      let list = [
+        'Kratos',
+        'Deredeo Dreadnought',
+        'Relic Contemptor Dreadnought',
+        'Leviathan Dreadnought',
+        'Land Raider Proteus',
+        'Land Raider Achilles',
+        'Sicaran Battle Tank',
+        'Sicaran Venator',
+        'Sicaran Punisher',
+        'Rapier Carrier',
+        'Whirlwind Scorpius',
+        'Terrax-pattern Termite',
+        'Xiphon Interceptor',
+        'Storm Eagle Gunship',
+        'Fire Raptor Gunship',
+        'Typhon',
+        'Cerberus',
+        'Spartan',
+        'Fellblade',
+        'Falchion',
+        'Mastodon',
+        'Sokar-pattern Stormbird',
+      ];
+      let newData = {};
+      Object.entries(SM.rulebook.assetCatalog).forEach(([itemKey, item]) => {
         let [itemClass, itemDesignation] = itemKey.split('§');
-        Object.keys(item.stats || {}).forEach(statKey => {
-          let stat = item.stats[statKey];
-          if(typeof stat.value === 'string' && stat.value?.includes('D6')){
-            // regex to determine what die modifier exists. i.e. D6+1 = 1 while 2D6 = 0
-            let regex = /(\d*D\d)\+*(\d*)/g;
-            let match = regex.exec(stat.value);
-            let statFormat = match[1];
-            let statNum = Number(match[2]) || 0;
-            delete stat.statType;
-            stat.value = statNum;
-            stat.format = statFormat;
-          }
-        })
+        // check whether itemDesignation includes any elements of list
+        if (list.some(name => itemDesignation.includes(name))) {
+          newData[itemKey] = item;
+          item.assets?.traits.forEach(trait => {
+            let traitKey = trait.item ?? trait;
+            if(SM.rulebook.assetCatalog[traitKey]){
+              newData[traitKey] = SM.rulebook.assetCatalog[traitKey];
+            }
+          });
+          newData[itemKey].keywords = newData[itemKey].keywords || {};
+          newData[itemKey].keywords.Tags = newData[itemKey].keywords.Tags || [];
+          newData[itemKey].keywords.Tags.push('chaosShared');
+        } else {
+          // itemDesignation does not include any elements from the list
+          // do something else
+        }
       });
-      console.log(data);
+      Chaos.rulebook.assetCatalog = {...newData,...Chaos.rulebook.assetCatalog};
+      list = [
+        'Chaos Thunderhawk',
+        'Greater Blight Drone',
+        'Blood Slaughterer',
+        'Greater Brass Scorpion',
+        'Kytan Ravager',
+        'Chaos Lord on Palanquin of Nurgle',
+        'Gellerpox Infected',
+        'Hell Blade',
+        'Hell Talon',
+        'Mutoid Vermin',
+        'Sorcerer on Palanquin of Nurgle',
+        'Chaos Lord on Disc of Tzeentch',
+      ];
+      newData = {};
+      Object.entries(CSM.rulebook.assetCatalog).forEach(([itemKey, item]) => {
+        let [itemClass, itemDesignation] = itemKey.split('§');
+        // check whether itemDesignation includes any elements of list
+        if (list.some(name => itemDesignation.includes(name))) {
+          newData[itemKey] = item;
+          delete CSM.rulebook.assetCatalog[itemKey];
+          item.assets?.traits.forEach(trait => {
+            let traitKey = trait.item ?? trait;
+            if(CSM.rulebook.assetCatalog[traitKey]){
+              newData[traitKey] = CSM.rulebook.assetCatalog[traitKey];
+            }
+          });
+          newData[itemKey].keywords = newData[itemKey].keywords || {};
+          newData[itemKey].keywords.Tags = newData[itemKey].keywords.Tags || [];
+          newData[itemKey].keywords.Tags.push('chaosShared');
+        } else {
+          // itemDesignation does not include any elements from the list
+          // do something else
+        }
+      });
+      Chaos.rulebook.assetCatalog = {...newData,...Chaos.rulebook.assetCatalog};
+      console.log(CSM);
+      console.log(Chaos);
     } catch (error) {
       // Handle any error that occurs during loading
       console.error(error);
     }
-  }
+  // }
 }
 
 processFiles();
